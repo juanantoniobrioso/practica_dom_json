5ea3eb2cd5540d8c4623b4ffeac30d86ac3d06c2
Juan Antonio <juanantoniobrioso.24@campuscamara.es>
2025-11-09 23:42:06 +0100

    entrega

---
+++ README.md
@@ -0,0 +1,7 @@
+# Práctica DOM JSON
+
+En esta práctica se nos dan unos cuantos ejercicios relacionados con la
+manipulación del DOM mediante Javascript. En esta práctica debemos
+modificar elementos, sus textos, sus colores, crear elementos, imagenes, etc.
+
+_Juan Antonio Brioso García_
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //1. Acceso al DOM básico
 const textoEncabezado = document.getElementById(encabezado);
 const colorParrafo = document.getElementsByTagName("p");
@@ -15,3 +16,29 @@ for (let i = 0; i < textos.length; i++){
 //3. Selección con querySelector
 const caja = document.querySelector(".caja");
 caja.style.backgroundColor = "lightblue";
+
+//4. querySelectorAll y forEach
+const botones = document.querySelectorAll('.btn-dom');
+botones.forEach(boton => {
+    boton.textContent = "DOM listo";
+}); //no funciona
+
+//5. Comparación entre colecciones vivas y estáticas
+const lista = document.getElementById('lista');
+
+//6. Crear elementos dinámicamente
+const parrafo = document.createElement('p');
+parrafo.textContent = "Creado dinámicamente";
+
+document.body.appendChild(parrafo); //no funciona
+
+//7. 
+const nodoTexto = document.createTextNode('Texto generado con createTextNode');
+const contenedor = document.getElementsByClassName('contenedor');
+contenedor.body.appendChild(nodoTexto); //no funciona
+
+//8.
+const imagen = document.createElement('img');
+imagen.setAttribute('src', 'https://media.istockphoto.com/id/1434983638/es/foto/conejo-europeo-joven-frente-a-la-c%C3%A1mara-oryctolagus-cuniculus.jpg');
+imagen.setAttribute('alt', 'Imagen generada por JS');
+document.body.appendChild(imagen);
